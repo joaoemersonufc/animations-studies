c14507b70cbbf9d95312da336d2f0466d7111eca
joaoemersonufc <joaoemersonufc@gmail.com>
2025-10-04 22:31:48 -0300

    feat: adding some animations

---
+++ animations/drag-and-drop-ball.js
@@ -0,0 +1,20 @@
+"use client";
+ 
+import { motion } from "framer-motion";
+import { useRef } from "react";
+ 
+export function DragExample() {
+  const boundingBox = useRef(null);
+ 
+  return (
+    <div ref={boundingBox} className="h-64 w-full p-6">
+      <motion.div
+        drag
+        // this prevents the element from being dragged outside of the bounding box
+        dragConstraints={boundingBox}
+		dragMomentum={false}
+        className="h-10 w-10 rounded-full bg-gray-400"
+      />
+    </div>
+  );
+}
